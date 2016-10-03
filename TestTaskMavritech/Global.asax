9567c5ad5116212565eb2242e6ac83e36c74d826
nastafirik <nastafirik@gmail.com>
2016-10-03 04:02:01 +0300

    test

---
+++ TestTaskMavritech/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestTaskMavritech.MvcApplication" Language="C#" %>
---
+++ TestTaskMavritech/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestTaskMavritech.Startup))]
+namespace TestTaskMavritech
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
